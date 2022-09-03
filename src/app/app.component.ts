c03572036453a5816fd1e88c1d929936b6c64693
ellievi <residentamv@hotmail.com>
2022-09-02 23:09:31 -0300

    Update json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+declare function submitform(): void;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gamemania';
+
+  constructor(){;
+  }
 }

10582abea0fed3942f314179689bc17be4ab2d4f
ellievi <residentamv@hotmail.com>
2022-09-18 21:31:46 -0300

    Update code

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,15 +9,24 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
-    userModel = new User ();
+  userModel = new User ();
+
+  mensagem = ""  
+
     receberDados() {
       console.log(this.userModel)
       this.loginService.login(this.userModel).subscribe((response) => {
-          console.log(response)
+          console.log("Deu certo!")
+          localStorage.setItem("nomeUsuario", response.body.user.nome)
+
+          this.router.navigateByUrl("/")
+      }, (respostaErro) => {
+        console.log("Error")
+        alert(respostaErro.error)
       })
     }
 }
